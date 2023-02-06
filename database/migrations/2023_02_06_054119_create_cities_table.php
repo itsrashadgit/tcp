45905d8fb1a23cb213414f7634319820addfadd6
Salim Hosen <salim7cse@gmail.com>
2023-02-06 11:48:47 +0600

    new database added

---
+++ database/migrations/2023_02_06_054119_create_cities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("state_id")->constrained()->onDelete("cascade");
+            $table->string("city", 50);
+            $table->string("county", 50);
+            $table->double("latitude", 15, 2);
+            $table->double("longitude", 15, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+};
